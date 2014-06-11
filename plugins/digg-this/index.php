dceb3bb4a4a1bcb670f0ca4935217953ad1342c1
wahyusoft <wahyusoft@yahoo.com>
2014-06-11 11:15:16 +0700

    add plugin this

---
+++ plugins/digg-this/index.php
@@ -0,0 +1,16 @@
+<?php
+/* Information Header Goes here */ 
+// include() or require() any necessary files here...
+// Settings and/or Configuration Details go here...
+// Tie into WordPress Hooks and any functions that should run on load.
+// "Private" internal functions named with a leading underscore
+function _diggthis_get_post_description() { }
+function _diggthis_get_post_media_type() { }
+function _diggthis_get_post_title() { }
+function _diggthis_get_post_topic() { }
+function _diggthis_get_post_url() { }
+// The "Public" functions
+function diggthis_add_js_to_doc_head() { }
+function diggthis_check_wordpress_version() { }
+function diggthis_get_button() { }
+/* EOF */
